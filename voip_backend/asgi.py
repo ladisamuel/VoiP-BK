569fcc2008ac937c3443d6ed440ada499c394dab
SupryTech <aegis_corporation@outlook.com>
2026-05-26 04:33:12 -0700

    fix: fixed undefined render hostname

--- voip_backend/asgi.py
+++ voip_backend/asgi.py
@@ -2,13 +2,16 @@
 import os
 import django
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voip_backend.settings")
+
+django_asgi_app = get_asgi_application()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from calls.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": URLRouter(websocket_urlpatterns),
 })
